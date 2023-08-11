2aa2b9fdba91b3c08246e41148f6162f77608b8a
ahsanulkholikin <ahsanulkholikin99@gmail.com>
2023-08-11 08:20:06 +0700

    btw sorry grafiknya gk rapih

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Page Title' }} | {{ config('app.name') }}</title>
-    {{-- <link href="https://cdn.jsdelivr.net/npm/beercss@3.2.13/dist/cdn/beer.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/beercss@3.2.13/dist/cdn/beer.min.css" rel="stylesheet">
     <script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.2.13/dist/cdn/beer.min.js"></script>
     <script type="module"
-        src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.0.1/dist/cdn/material-dynamic-colors.min.js"></script> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @stack('styles')
+        src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.0.1/dist/cdn/material-dynamic-colors.min.js"></script> 
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js'])  --}}
+    @stack('styles') 
     <livewire:styles />
 </head>
 
